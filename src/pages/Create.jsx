b82872135562717e6d41f33f2022ca0fcc45f714
Pradyumna <hariharshasripradhyumna@gmail.com>
2024-04-09 02:04:54 +0530

    second commit

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { BiMenuAltLeft } from 'react-icons/bi'
 
 const Create = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    setOpen(open);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <div>
-        <Button>
-            Click Here
+        <Button onClick={toggleDrawer(true)} variant='contained' color='secondary' style={{position: 'fixed', top: '8px', borderRadius: '100%', height: "2.5rem", width: "2.5rem"}}>
+          <BiMenuAltLeft size = {'20'} />
         </Button>
+
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          {DrawerList}
+        </Drawer>
     </div>
-  )
-}
+  );
+};
 
 export default Create;
